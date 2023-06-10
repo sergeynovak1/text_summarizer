b3b15c10a6d3c969b93eddc36ae7476d45f969ef
Sergey <ssnovak@stud.kpfu.ru>
2023-06-10 12:29:42 +0300

    + styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,64 +1,90 @@
 <template>
-  <div>
-  <h1>Краткое содержание текста</h1>
-  <br>
-  <div class="full-width-div">
-    <b-form @submit.prevent="onSubmit">
-    <b-form-group
-              label="Введите текст, который нужно сократить:"
-              label-for="input-1"
-          >
-    <b-form-input id="input-1" v-model="text" placeholder="Начинайте вводить текст..."></b-form-input>
-    </b-form-group>
-      <b-button type="submit" variant="primary">Отправить</b-button>
-    </b-form>
-  </div>
 
-  <br>
-  <div>
-  <b-row>
+  <div className="main">
+    <h1>Краткое содержание текста</h1>
+    <br>
+    <div className="full-width-div">
+      <b-form @submit.prevent="onSubmit">
+        <b-form-group className="custom-label"
+                      label="Введите текст, который нужно сократить:"
+                      label-for="input-1"
+        >
+          <b-form-textarea className="custom-input autosize w-100" id="input-1" v-model="text"
+                           placeholder="Начинайте вводить текст..."></b-form-textarea>
+        </b-form-group>
+        <b-button style="margin: 10px" type="submit" variant="primary">Отправить</b-button>
+
+        <b-button @click="this.text=''" type="submit" variant="primary">Стереть</b-button>
+      </b-form>
+    </div>
+
+    <br>
+    <div>
+      <b-spinner v-if="this.loading"></b-spinner>
+      <b-row>
         <b-col>
-  <TextCardView :text="this.results_first" :title="'Частотный анализ'"/>
-  </b-col>
-    <b-col>
-          <TextCardView :text="this.results_second" :title="'Косинусное расстояние'"/>
-  </b-col>
-    <b-col>
-      <TextCardView :text="this.results_third" :title="'TF IDF'"/>
+          <TextCardView :text="this.results_first" :title="'Частотный анализ'"/>
         </b-col>
-  </b-row>
+        <b-col>
+          <TextCardView :text="this.results_second" :title="'Косинусное расстояние'"/>
+        </b-col>
+        <b-col>
+          <TextCardView :text="this.results_third" :title="'TF IDF'"/>
+        </b-col>
+      </b-row>
     </div>
   </div>
 </template>
 
 <script>
-  import {getSummurytext} from "./services/api";
-  import TextCardView from "./components/TextCardView.vue";
+import {getSummurytext} from "./services/api";
+import TextCardView from "./components/TextCardView.vue";
+import autosize from "autosize/dist/autosize";
 
-  export default {
-    components: {TextCardView},
-    data() {
-      return {
-        text: '',
-        results_first: '',
-        results_second: '',
-        results_third: ''
-      }
-    },
-    methods: {
-      async onSubmit() {
+export default {
+  components: {TextCardView},
+  mounted() {
+    autosize(document.querySelectorAll('.autosize'));
+  },
+  data() {
+    return {
+      loading: false,
+      text: '',
+      results_first: '',
+      results_second: '',
+      results_third: ''
+    }
+  },
+  methods: {
+    async onSubmit() {
+      if (this.text) {
+        this.loading = true
         this.results_first = await getSummurytext(1, this.text)
         this.results_second = await getSummurytext(2, this.text)
         this.results_third = await getSummurytext(3, this.text)
+        this.loading = false
       }
     }
   }
+}
 </script>
 
 <style scoped>
-  .full-width-div {
-  width: 100%;
+html,
+body {
   margin: 0;
   padding: 0;
+  font-family: sans-serif;
+}
+
+
+.full-width-div {
+  width: 100%;
+}
+
+
+.main {
+  width: 95%;
+  margin-left: 1rem;
 }
 </style>
